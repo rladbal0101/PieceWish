a7ffcecdaec025dd04e5bc27b2c7c5d4074c54d4
rladbal0101 <rladbal0101@naver.com>
2023-04-27 19:00:25 +0900

    꽃송이만들기, 서브페이지 구현, 메인메뉴-서브페이지 링크연결

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ new Swiper('.slide .swiper', {
   // direction: 'horizontal', // 수평 슬라이드(기본값)
   loop: true, // 반복 재생 여부
   autoplay: {
-    delay: 3000 // 5초마다 슬라이드 바뀜(기본값: 3000)
+    delay: 3000 // 3초마다 슬라이드 바뀜(기본값: 3000)
   }, // 자동 재생 여부
   slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수(기본값: 1)
   spaceBetween: 20, // 슬라이드 사이 여백(간격) px
@@ -39,6 +39,24 @@ new Swiper('.slide .swiper', {
   navigation: { // 슬라이드 이전/다음 버튼 사용
     nextEl: '.slide .swiper-button-next',
     prevEl: '.slide .swiper-button-prev',
+  },
+});
+
+// 서브페이지 수평슬라이드
+new Swiper('.order__contents .swiper', {
+  loop: true,
+  autoplay: {
+    delay: 4000
+  },
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수(기본값: 1)
+  spaceBetween: 20, // 슬라이드 사이 여백(간격) px
+  pagination: { // 페이지 번호 사용
+    el: '.order__contents .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: { // 슬라이드 이전/다음 버튼 사용
+    nextEl: '.order__contents .swiper-button-next',
+    prevEl: '.order__contents .swiper-button-prev',
   },
 });
 
